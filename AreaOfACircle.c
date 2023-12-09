abbc1cff1f45852105818049359341263f3008ca
Dhrubaraj Pati <146111647+codewithdhruba01@users.noreply.github.com>
2023-12-09 11:54:19 +0530

    Create AreaOfACircle.c

---
+++ AreaOfACircle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    float redius;
+    printf("Plz Enter Redius : ");
+    scanf("%f", &redius);
+    float pi = 3.1415;
+    float area = pi * redius * redius;
+    printf("The area of a circle is : %f", area);
+  printf("\nThank You");
+    return 0;
+}
