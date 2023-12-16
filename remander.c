51688d880d639ac73c7f30d7ce472f117e2f3be5
Dhrubaraj Pati <146111647+codewithdhruba01@users.noreply.github.com>
2023-12-16 11:09:47 -0800

    Add files via upload

---
+++ remander.c
@@ -0,0 +1,14 @@
+/*remander nikalna*/
+#include<stdio.h>
+int main() {
+    int a, b; //b > a
+    printf("Enter Dividend Number : ");
+    scanf("%d", &a);
+     printf("Enter Divisor Number : ");
+    scanf("%d", &b);
+    int q = a/b;
+    int r = a-(b*q); //Divisor * Quotient + Remainder = Dividend
+    printf("The remander when %d is divided by %d is : %d",a,b,r);
+    printf("\n Thanks ");
+return 0;
+}
